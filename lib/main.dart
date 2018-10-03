be9a88030f15a8d0008fc5fc632f804a254290b5
Gilles Coolen <gilleswhc@gmail.com>
2018-10-03 10:15:49 +0200

    Added simple drawer.

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,43 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Octoscan',
-      theme: new ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: new Scaffold(appBar: AppBar(title: Text('Octoscan'))),
-    );
+    return MaterialApp(
+        title: 'Octoscan',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Octoscan', style: TextStyle(color: Colors.white)),
+            iconTheme: IconThemeData(color: Colors.white),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('Kasteel Huys ter Hof',
+                      style: TextStyle(color: Colors.white)),
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                  ),
+                ),
+                ListTile(
+                  title: Text('Scan History'),
+                  onTap: () {
+                    // TODO: Add scan history page.
+                  },
+                ),
+                ListTile(
+                  title: Text('Events'),
+                  onTap: () {
+                    // TODO: Add events page.
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }

738518329bde1d881287cdc3b8291f5cc9df116a
Unknown <gilleswhc@gmail.com>
2018-10-27 15:52:16 +0200

    Add working error screen

--- lib/screens/error.dart
+++ lib/screens/error.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:huys_ter_horst/screens/home.dart';
 
 class ErrorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("An error has occured.");
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              padding: EdgeInsets.only(bottom: 81.0),
+              child: Center(
+                child: Text(
+                  'Er is iets misgegaan!',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              )),
+          RaisedButton(
+            color: Colors.orange,
+            child: Text(
+              'Klik om terug te gaan.',
+              style: TextStyle(fontSize: 16.0, color: Colors.white),
+            ),
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainPage()),
+                  ModalRoute.withName(Navigator.defaultRouteName));
+              /* Navigator.popUntil(
+                  context, ModalRoute.withName(Navigator.defaultRouteName)); */
+            },
+          )
+        ],
+      ),
+    );
   }
 }

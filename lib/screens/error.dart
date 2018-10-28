a9cf8fb9cb4f9050b3c08d75b99decb4dc7f1b06
Haydar Pehlivan <haydarpehlivan61@hotmail.com>
2018-10-28 14:10:44 +0100

    Edit Error page

--- lib/screens/error.dart
+++ lib/screens/error.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class ErrorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("An error has occured.");
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Text("Foutmelding, Raadpleeg uw provider"),
+    );
   }
 }
